8062ad54e5e9582ddc00b3fcebb3d0760076d973
ASH78-lab <roben4300@gmail.com>
2026-09-08 18:11:16 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -564,7 +564,7 @@ def pin_all():
     df = pd.DataFrame(data, columns=header)
     df = df.loc[df['H'] != '-']
   
-    df.loc[df['X'] == '-', 'X'] = 15=
+    df.loc[df['X'] == '-', 'X'] = 15
     df.loc[(df['1'] == '-') & (df['handicap'].isin(['-']), '1'] = 1.01
     df.loc[(df['2'] == '-') & (~df['handicap'].isin(['-'])), '2'] = 30
     df.loc[(df['1'] == '-') & (~df['handicap'].isin(['-'])), '1'] = 30
